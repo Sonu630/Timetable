75185eda3b4f434120b9cdf4447bc44836c4362f
Sonu3068 <me240003068@iiti.ac.in>
2026-06-05 17:26:44 +0530

    vercel frontend  url

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin:["http://localhost:3000",
+      "https://timetablemanagementsystem-three.vercel.app/"],
     methods: ["GET", "POST"],
   },
 });
